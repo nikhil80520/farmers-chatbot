c1fe58ba739e8bfd08a1ddd6357bd1dc08e9cded
Nikhil kumar <143409903+nikhil80520@users.noreply.github.com>
2024-10-18 12:19:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-from flask import Flask
 from flask import Flask, request, make_response
 import os, json
-from flask_cors import CORS,cross_origin
-from weather_data import WeatherData
+from flask_cors import CORS, cross_origin
+from weather_data import WeatherData  # Ensure this import is correct
 
 app = Flask(__name__)
+CORS(app)  # Enabling CORS for all routes
+
+# Initialize an instance of WeatherData globally
+weather_data_instance = WeatherData()
 
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
 
-# geting and sending response to dialogflow
+# Getting and sending response to Dialogflow
 @app.route('/webhook', methods=['POST'])
 @cross_origin()
 def webhook():
     req = request.get_json(silent=True, force=True)
-    print("Request:")
-    
-    print(json.dumps(req))
+    print("Request received:")
+    print(json.dumps(req, indent=4))
 
-    res = object.processRequest(req)
-
+    # Use the instance of WeatherData to call processRequest
+    res = weather_data_instance.processRequest(req)  # Fixed this line
     res = json.dumps(res)
+    
+    print("Response to be sent:")
     print(res)
+    
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-#if __name__ == '__main__':
- #   object=WeatherData()
-    #port = int(os.getenv('PORT', 5000))
-    #print("Starting app on port %d" % port)
-  #  app.run(debug=True)
-if __name__=='__main__':
-      app.run(host='0.0.0.0',port=8080)
+if __name__ == '__main__':
+    # Run the app in debug mode
+    app.run(debug=True)
