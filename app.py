229686c95b6759da03e6de668ab7705ba2c4453f
Nikhil kumar <143409903+nikhil80520@users.noreply.github.com>
2024-10-18 13:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import Flask, request, make_response
 import os, json
 from flask_cors import CORS,cross_origin
-from weather_data import WeatherData
+from weather.weather_data import WeatherData
 
 app = Flask(__name__)
 
